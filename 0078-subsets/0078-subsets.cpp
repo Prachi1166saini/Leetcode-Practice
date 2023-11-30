db239ac99ed94d92a4cce8306d6bffc36d9597a1
Prachi1166saini <143393488+Prachi1166saini@users.noreply.github.com>
2023-11-30 23:02:53 +0530

    Time: 4 ms (37.76%), Space: 17.1 MB (16.33%) - LeetHub

---
+++ 0078-subsets/0078-subsets.cpp
@@ -0,0 +1,30 @@
+class Solution {
+    private:
+    void solve(vector<int> nums,int index, vector<vector<int>> &ans, vector<int> output){
+        //base case 
+        if(index>=nums.size()){
+            ans.push_back(output);
+            return;
+        }
+        
+        //exclude
+        solve(nums,index+1, ans, output);
+        
+        //include
+        int element=nums[index];
+        output.push_back(element);
+        solve(nums, index+1, ans, output);
+    }
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> ans;
+        vector<int> output;
+        int index=0;
+        solve(nums, index, ans, output);
+        
+        return ans;
+        
+        
+        
+    }
+};
